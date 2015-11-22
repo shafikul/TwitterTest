3eb5feddd0141ebe4827c692abdb6463b3460024
Md. Shafikul Islam <sagar.shunno@gmail.com>
2015-11-22 19:01:53 +0000

    First Sign In

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   get '/profile' => 'pages#Profile'
 
   get '/explore' => 'pages#Explore'
+  
+  get '/signup' => 'users#new'
+  
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
