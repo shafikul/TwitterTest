c5009c813f7104008294e2bdbd3671b297283142
Md. Shafikul Islam <sagar.shunno@gmail.com>
2015-11-23 18:59:14 +0000

    updated Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:edit,:update] 
+    before_action :correct_user, only: [:edit, :update]
     
     def show
        @user =	User.find(params[:id])
@@ -15,18 +17,37 @@ class UsersController < ApplicationController
         redirect_to	 @user
     else
     render 'new'
-    
+    end
     end
     
     def edit
-        puts "hit"
-        @user = User.find 6
+        @user = User.find_by_id(params[:id])
     end
     
+    def update
+        @user =	User.find_by_id(params[:id])
+        if	@user.update_attributes(user_params)
+        flash[:success]	= "Profile updated"
+        redirect_to	@user
+        else
+        render	'edit'
+        end
     end
     
     private
     def user_params
         params.require(:user).permit(:fullname,	:nickname, :email,	:password, :password_confirmation)
     end
+    def	logged_in_user
+    unless	logged_in?
+        flash[:danger]	=	"Please	log	in."
+        redirect_to	login_url
+    end
+    end
+    
+    def	correct_user
+        @user =	User.find_by_id(params[:id])
+        redirect_to(root_url) unless @user == current_user
+    end
+    
 end
